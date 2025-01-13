33c723c8f9e73fab9c63191de9f864ee35ef5756
Tarun Sharma <tarunsharma@192.168.1.14>
2025-01-13 13:58:34 +0530

    env Changes

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -12,39 +12,40 @@
  */
 
 import {
-	GoogleGenerativeAI,
-	HarmCategory,
-	HarmBlockThreshold,
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
 } from "@google/generative-ai";
 
-// const apiKey = process.env.GEMINI_API_KEY;
-const apiKey = "AIzaSyD5xrg3xmb6O_T019y_E0H5c6yww4hgcTE";
+// console.log(import.meta.env.VITE_GEMINI_API_KEY);
+
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-	model: "gemini-1.5-flash",
+  model: "gemini-1.5-flash",
 });
 
 const generationConfig = {
-	temperature: 1,
-	topP: 0.95,
-	topK: 64,
-	maxOutputTokens: 8192,
-	responseMimeType: "text/plain",
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
 };
 
 async function runChat(prompt) {
-	const chatSession = model.startChat({
-		generationConfig,
-		// safetySettings: Adjust safety settings
-		// See https://ai.google.dev/gemini-api/docs/safety-settings
-		history: [],
-	});
+  const chatSession = model.startChat({
+    generationConfig,
+    // safetySettings: Adjust safety settings
+    // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [],
+  });
 
-	const result = await chatSession.sendMessage(prompt);
-	const response = result.response;
-	console.log(response.text());
-	return response.text();
+  const result = await chatSession.sendMessage(prompt);
+  const response = result.response;
+  console.log(response.text());
+  return response.text();
 }
 
 export default runChat;
